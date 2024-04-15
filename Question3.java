712ed2af6ee4ed79bfb699d00704fa678830b6c8
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-04-15 15:08:03 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,7 +11,6 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.printIn("Enter an integer: ");
     int num = in.nextInt();
     int result = num * num;
     System.out.printIn(result);
