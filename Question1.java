7fadaaed404dbb15b26694f5f65d0f1977491166
xuewenya <148406569+xuewenya@users.noreply.github.com>
2024-04-15 14:12:54 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,12 +1,6 @@
 public class Question1
 {
-  public static void main(String[] args)
-  {
-    System.out.printIn("Hello World!");
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
   }
 }
